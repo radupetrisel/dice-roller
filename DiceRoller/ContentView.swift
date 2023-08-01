3ed0c777e6e9afd48f66f616df9c9f6c6afb6a91
Radu Petrisel <radupetrisel@gmail.com>
2023-08-01 15:11:22 +0300

    Add basic dice functionality

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var roll = Int.random(in: 1...6)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Current roll:")
+                    .font(.title2)
+                    .foregroundStyle(.secondary)
+                
+                Text("\(roll)")
+                    .font(.largeTitle)
+                    .padding()
+                
+                Button("Roll") {
+                    roll = Int.random(in: 1...6)
+                }
+                .frame(width: 90, height: 50)
+                .background(.blue)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .clipShape(Capsule())
+            }
         }
-        .padding()
     }
 }
 
